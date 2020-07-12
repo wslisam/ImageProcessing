1abfb002634fedc64bec7d65de4fb1746cfc7f8d
Sam Li <55217028+wsli1998@users.noreply.github.com>
2020-07-12 12:58:30 +0800

    window to mac

---
+++ build/CMakeFiles/startProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/startProject.dir/main.cpp.o"
+  "startProject"
+  "startProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/startProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
