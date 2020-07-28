bc860be871b34a31b954d96d8f0c5b2527edbc08
Sam Li <55217028+wsli1998@users.noreply.github.com>
2020-07-28 22:23:13 +0800

    gitinore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Sam Li/C-Project/ImageProcessing
-# Build directory: C:/Users/Sam Li/C-Project/ImageProcessing/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
